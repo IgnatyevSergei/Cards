2e00fa5dac830f405385e4710e80053ce9f49265
AfanasenkoYulka <afanasenkoyulka94@gmail.com>
2022-07-04 20:45:45 +0300

    add

--- script.js
+++ script.js
@@ -1,2 +1,52 @@
-console.log(1
-)
+const getAnswer = (index) => {
+    const answer = ['1', '2', '3', '4', '5']
+    return answer[index]
+}
+
+
+const cards = [
+    {
+        input: document.querySelector('#input1'),
+        btn: document.querySelector('.btn1')
+    },
+    {
+        input: document.querySelector('#input2'),
+        btn: document.querySelector('.btn2')
+    },
+    {
+        input: document.querySelector('#input3'),
+        btn: document.querySelector('.btn3')
+    },
+    {
+        input: document.querySelector('#input4'),
+        btn: document.querySelector('.btn4')
+    },
+    {
+        input: document.querySelector('#input5'),
+        btn: document.querySelector('.btn5')
+    }
+]
+
+let counter = 0
+
+cards
+    .sort(() => Math.random() - 0.5)
+    .forEach((card, index) => {
+        card.btn.addEventListener('click', handlerCheckAnswer.bind(card, index))
+    })
+
+function handlerCheckAnswer(index) {
+    if (this.input.value === getAnswer(index)) {
+        this.input.disabled = true
+        this.btn.disabled = true
+        counter++
+        if (counter === 5) {
+            alert("Вы ответили правильно")
+            window.location.href = 'https://google.com'
+        }
+    } else {
+        this.input.value = ''
+        alert('Ответ неверный')
+    }
+
+}
